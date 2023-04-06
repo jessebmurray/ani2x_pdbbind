218b79640102b1f5b8c547c20d00f5b66f382204
jessebmurray <jessebmurray1@gmail.com>
2023-04-06 16:46:04 +0100

    change lr

--- run_ani2x.sh
+++ run_ani2x.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --nodes=1                       # number of nodes                                       
-#SBATCH --ntasks=1                      # 1 tasks                                               
-#SBATCH --gres=gpu:1                    # number of gpus
+#SBATCH --nodes=1                       # number of nodes
+#SBATCH --ntasks=1                      # 1 tasks
+#SBATCH --gres=gpu:2                    # number of gpus
 #SBATCH --gpus-per-task=1               # number of gpus per task
 #SBATCH --cpus-per-gpu=1                # number of cores per gpu
 #SBATCH --mem-per-cpu=10000             # memory/cpu (in MB)
--- run_ani2x_rand.py
+++ run_ani2x_rand.py
@@ -1,7 +1,7 @@
 from ani2x import *
 
 def main():
-    train_rand_models(batchsize=26, epochs=150, lr_rand=1e-5)
+    train_rand_models(batchsize=26, epochs=150, lr_rand=1e-4)
 
 if __name__ == "__main__":
     main()
